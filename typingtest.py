8e848092d1802390b2afcd36721f14e8760a4451
Bryceg-26 <bryce.gold8@gmail.com>
2025-01-07 11:42:55 -0700

    Typing Test

---
+++ typingtest.py
@@ -0,0 +1,17 @@
+import time
+
+def main():
+    sent="the quick brown fox jumps over the lazy dog"
+    words=input("type 'the quick brown fox jumps over the lazy dog':")
+    if words==sent:
+        print("correct")
+    else:
+        print("Incorrect, retry.")
+
+#def timer():
+ #   t = 0
+  #  while True:
+   #     timr=t+1
+    #    time.sleep(1)
+
+main()
